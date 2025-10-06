f51ff10f0ab7e30950402149d2093b229f342bdf
Tejas59 <57625719+Tejas59@users.noreply.github.com>
2025-10-06 12:35:36 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes/index";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+dotenv.config();
+
+connectDB();
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    })
+);
+
+app.use(process.env.APP_PREFIX_PATH ?? "/api", routes);
+
+const APP_PORT = process.env.APP_PORT || 5000;
+app.listen(APP_PORT, () => {
+    console.log(`server is running on the port ${APP_PORT}`);
+});
---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,25 @@
+import {
+    loginFun,
+    logoutFun,
+    registrationFun,
+} from "../controllers/authController"
+import express, { Request, Response } from "express";
+
+const auth = express.Router();
+
+auth.post("/register", async (req: Request, res: Response) => {
+    const result = await registrationFun(req, res);
+    res.status(200).json(result);
+});
+
+auth.post("/login", async (req: Request, res: Response) => {
+    const result = await loginFun(req, res);
+    res.status(200).json(result);
+});
+
+auth.get("/logout", async (req: Request, res: Response) => {
+    const result = await logoutFun(res);
+    res.status(200).json(result);
+});
+
+export default auth;
